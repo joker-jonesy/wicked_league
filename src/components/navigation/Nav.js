5a4c7227b8e2166f4ceef89923e2f626527281c8
Luke Jones <11872906+joker-jonesy@users.noreply.github.com>
2021-02-12 15:05:25 -0500

    Updated Navigation

--- src/components/navigation/Nav.js
+++ src/components/navigation/Nav.js
@@ -1,43 +1,11 @@
 import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import Logo from './../../assets/images/villainousLogoWhite.png'
-import {Link, useHistory} from "react-router-dom";
-import Villains from "../../data/VIllainData";
+import {Link} from "react-router-dom";
+
 
 function Nav() {
 
-    //redux state goes here
 
-    // const dispatch = useDispatch();
-
-    let VillainPage=false;
-
-    let goHome;
-
-    const history = useHistory();
-
-    React.useEffect(()=>{
-
-        return history.listen((location) => {
-            for(let i=0; i< Villains.length; i++){
-                if (window.location.href.indexOf(Villains[i].title) > -1) {
-                    VillainPage=true;
-                }else{
-                    VillainPage=false;
-                }
-            }
-        });
-
-
-    },[history]);
-
-    if(VillainPage){
-        goHome=<Link to={"/"} className={"ham"} >
-
-            <FontAwesomeIcon icon='undo' size="3x" />
-
-        </Link>
-    }
 
 
 
@@ -52,7 +20,6 @@ function Nav() {
             <Link to={"/"} className={"logo"} >
                 <img src={Logo} alt={"Villainous_Logo"}/>
             </Link>
-            {goHome}
 
         </nav>
 
